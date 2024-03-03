f55487dd0da7a433cba87e1910f6b6fe3648ac02
sid <siddharth.singh@gmail.com>
2024-03-04 02:36:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import movieImage from './Movie1.jpg';
+import { Container, CssBaseline, Paper } from '@mui/material';
+import SchindlersListImage from './SchndilersList.jpg';
+import TheBucketListImage from './TheBucketList.jpg';
+
 
 const playMovie = (url) => {
   window.open(url, '_blank'); // Opens the movie in a new window
@@ -8,35 +11,38 @@ const playMovie = (url) => {
 
 const movies = [
   {
-    title: 'Movie 1',
-    image: require('./Movie1.jpg'), // Replace with actual image path
-    url: 'https://github.com/SimpSid/Movie/blob/main/Superbad.2007.Unrated.BrRip.720p.264.YIFY.mp4', // Replace with actual GitHub raw video URL
-  }
-  // {
-  //   title: 'Movie 2',
-  //   image: require('./movie2.jpg'), // Replace with actual image path
-  //   url: 'https://github.com/your-username/your-repository/raw/main/movie2.mp4', // Replace with actual GitHub raw video URL
-  // },
+    title: 'Schindlers List',
+    image: require('./SchndilersList.jpg'), // Replace with actual image path
+    url: 'https://youtu.be/d6_sxkfjUVk', // Replace with actual GitHub raw video URL
+  },
+  {
+    title: 'The Bucket List',
+    image: require('./TheBucketList.jpg'), // Replace with actual image path
+    url: 'https://youtu.be/8Iq4x6aYDs0', // Replace with actual GitHub raw video URL
+  },
   // Add more movie objects as needed
 ];
 function App() {
   return (
-    <div className="App">
-    <header className="App-header">
-      {movies.map((movie, index) => (
-        <div key={index} className="Movie-container">
-          <img
-            src={movie.image}
-            className="Movie-image"
-            alt={movie.title}
-            onClick={() => playMovie(movie.url)}
-          />
-          <p>{movie.title}</p>
-        </div>
-      ))}
-    </header>
-  </div>
-);
+    <Container component="main" maxWidth="lg">
+      <CssBaseline />
+      <Paper elevation={0} className="background-paper">
+        <header className="App-header">
+          {movies.map((movie, index) => (
+            <div key={index} className="Movie-container">
+              <img
+                src={movie.image}
+                className="Movie-image"
+                alt={movie.title}
+                onClick={() => playMovie(movie.url)}
+              />
+              <p className="Movie-title">{movie.title}</p>
+            </div>
+          ))}
+        </header>
+      </Paper>
+    </Container>
+  );
 }
 
 export default App;
